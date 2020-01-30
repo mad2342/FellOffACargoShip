ad007832be4b2f045c6b18c1f33586fdada7ec51
Mad <github@typ03.de>
2020-01-30 23:31:26 +0100

    refactor

--- Source/FellOfACargoShip/FellOfACargoShip.cs
+++ Source/FellOfACargoShip/FellOfACargoShip.cs
@@ -17,24 +17,24 @@ namespace FellOfACargoShip
         internal static string LogPath;
         internal static string ModDirectory;
         internal static Settings Settings;
-
-        // BEN: Debug (0: nothing, 1: errors, 2:all)
-        internal static int DebugLevel = 2;
+        // BEN: DebugLevel (0: nothing, 1: error, 2: debug, 3: info)
+        internal static int DebugLevel = 1;
 
         public static void Init(string directory, string settings)
         {
             ModDirectory = directory;
-
             LogPath = Path.Combine(ModDirectory, "FellOfACargoShip.log");
-            File.CreateText(FellOfACargoShip.LogPath);
+
+            Logger.Initialize(LogPath, DebugLevel, ModDirectory, nameof(FellOfACargoShip));
 
             try
             {
                 Settings = JsonConvert.DeserializeObject<Settings>(settings);
             }
-            catch (Exception)
+            catch (Exception e)
             {
                 Settings = new Settings();
+                Logger.LogError(e);
             }
 
             // Harmony calls need to go last here because their Prepare() methods directly check Settings...
